2bff7327d91af838c4c6e953d83238727984a2da
Aliyu <sadiqful@gmail.com>
2023-05-14 12:13:32 +0100

    profile section

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,13 +8,21 @@ export default function Navbar() {
     return (
       <nav className="flex j-between">
         <div className="user">
-            <h2>Hello Sadiq</h2>
+            <h2>Hello Aliyu Abubakar</h2>
             <span>{new Date()}</span>
         </div>
 
         <div className="search-container flex a-center j-center gap b-rad-1 ">
            <BsSearch/> 
            <input type="text" placeholder="search" />
+        </div>
+
+        <div className="profile flex a-center gap">
+            <div className="notification">
+                <img src={bell} alt="Bell" />
+            </div>
+            <h4>Aliyu Abubakar</h4>
+            <img src={avatar} alt="Avatar" className="avatar" />
         </div>
 
       </nav>  
