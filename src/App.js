aa6f9921bc8ea0fe52a5af13d456b95cc5e68778
Aliyu <sadiqful@gmail.com>
2023-05-15 23:00:52 +0100

    added the components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-import React from 'react';
-import styled from "styled-components"
+import React from "react";
+import Dashboard from "./components/Dashboard";
+import Sidebar from "./components/Sidebar";
+import styled from "styled-components";
 
 export default function App() {
   return (
     <Container>
+      <Sidebar />
+      <Dashboard />
     </Container>
   );
 }
